78863bd84d80dc6659510ca285e91ffa7ddf43aa
Yuanhao Zhang <cercle@bu.edu>
2020-02-03 16:06:39 -0500

    before switch

--- components/unit/pickPhotoPrompt.js
+++ components/unit/pickPhotoPrompt.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
 
 const mapStateToProps = state => {
   return {
-    // currentPhotoIndex: state.currentPhotoIndex
+    currentPhotoIndex: state.currentPhotoIndex
   };
 };
 
