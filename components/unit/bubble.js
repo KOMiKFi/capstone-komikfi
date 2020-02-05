93b14a4abe39b86ab19d5b8bd30cbafb41b41fa0
Yuanhao Zhang <cercle@bu.edu>
2020-02-05 14:48:36 -0500

    able to detect to two finger

--- components/unit/bubble.js
+++ components/unit/bubble.js
@@ -24,26 +24,25 @@ class Bubble extends React.Component {
   constructor(props) {
     super(props);
     //pinch
-    this._baseScale = new Animated.Value(1);
-    this._pinchScale = new Animated.Value(1);
-    this._scale = Animated.multiply(this._baseScale, this._pinchScale);
-    this._lastScale = 1;
-    this._onPinchGestureEvent = Animated.event(
-      [{ nativeEvent: { scale: this._pinchScale } }],
-      { useNativeDriver: true }
-    );
-    _onPinchHandlerStateChange = event => {
-      if (event.nativeEvent.oldState === State.ACTIVE) {
-        this._lastScale *= event.nativeEvent.scale;
-        this._baseScale.setValue(this._lastScale);
-        this._pinchScale.setValue(1);
-      }
-    };
+    // this._baseScale = new Animated.Value(1);
+    // this._pinchScale = new Animated.Value(1);
+    // this._scale = Animated.multiply(this._baseScale, this._pinchScale);
+    // this._lastScale = 1;
+    // this._onPinchGestureEvent = Animated.event(
+    //   [{ nativeEvent: { scale: this._pinchScale } }],
+    //   { useNativeDriver: true }
+    // );
+    // _onPinchHandlerStateChange = event => {
+    //   if (event.nativeEvent.oldState === State.ACTIVE) {
+    //     this._lastScale *= event.nativeEvent.scale;
+    //     this._baseScale.setValue(this._lastScale);
+    //     this._pinchScale.setValue(1);
+    //   }
+    // };
 
     this.panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-        console.log("inbubble", event, gesture);
         if (gesture.numberActiveTouches === 1) {
           this.setState({
             ...this.state,
@@ -55,7 +54,16 @@ class Bubble extends React.Component {
               gesture.dx * Math.sin(this.state.rotationZ)
           });
         } else if (gesture.numberActiveTouches === 2) {
-          console.log("detect two");
+          console.log("detect two", event.nativeEvent.changedTouches);
+          // this.setState({
+          //   ...this.state,
+          //   scaleX:
+          //     gesture.dx * Math.cos(this.state.rotationZ) +
+          //     gesture.dy * Math.sin(this.state.rotationZ),
+          //   translateY:
+          //     gesture.dy * Math.cos(this.state.rotationZ) -
+          //     gesture.dx * Math.sin(this.state.rotationZ)
+          // });
         }
       }
     });
@@ -71,47 +79,47 @@ class Bubble extends React.Component {
   }
   render() {
     return (
-      <PinchGestureHandler
-        ref={this.pinchRef}
-        simultaneousHandlers={this.rotationRef}
-        onGestureEvent={this._onPinchGestureEvent}
-        onHandlerStateChange={this._onPinchHandlerStateChange}
-      >
-        <View {...this.panResponder.panHandlers}>
-          <View
-            style={{
-              borderColor: "purple",
-              borderWidth: 5,
-              transform: [
-                { rotateZ: this.state.rotationZ },
-                // { rotateZ: `${this.state.rotationZ}deg` },
-                { translateX: this.state.translateX },
-                { translateY: this.state.translateY },
-                { scaleX: 1.5 },
-                { scaleY: 1.5 }
-              ]
-              // postition: "relative",
-              // transform: [{ translateX: this.state.translateX }]
-            }}
+      // <PinchGestureHandler
+      //   ref={this.pinchRef}
+      //   simultaneousHandlers={this.rotationRef}
+      //   onGestureEvent={this._onPinchGestureEvent}
+      //   onHandlerStateChange={this._onPinchHandlerStateChange}
+      // >
+      <View {...this.panResponder.panHandlers}>
+        <View
+          style={{
+            borderColor: "purple",
+            borderWidth: 5,
+            transform: [
+              { rotateZ: this.state.rotationZ },
+              // { rotateZ: `${this.state.rotationZ}deg` },
+              { translateX: this.state.translateX },
+              { translateY: this.state.translateY },
+              { scaleX: 1.5 },
+              { scaleY: 1.5 }
+            ]
+            // postition: "relative",
+            // transform: [{ translateX: this.state.translateX }]
+          }}
+        >
+          <ImageBackground
+            source={require("../../assets/bubble1.png")}
+            style={{ ...styles.bubble, height: this.props.height }}
           >
-            <ImageBackground
-              source={require("../../assets/bubble1.png")}
-              style={{ ...styles.bubble, height: this.props.height }}
-            >
-              <TextInput
-                style={{ ...styles.text, top: this.props.height / 5 }}
-                multiline
-                numberOfLines={2}
-                editable
-                onChangeText={text =>
-                  this.setState({ ...this.state, text: text })
-                }
-                value={this.state.text}
-              ></TextInput>
-            </ImageBackground>
-          </View>
+            <TextInput
+              style={{ ...styles.text, top: this.props.height / 5 }}
+              multiline
+              numberOfLines={2}
+              editable
+              onChangeText={text =>
+                this.setState({ ...this.state, text: text })
+              }
+              value={this.state.text}
+            ></TextInput>
+          </ImageBackground>
         </View>
-      </PinchGestureHandler>
+      </View>
+      // </PinchGestureHandler>
     );
   }
 }
