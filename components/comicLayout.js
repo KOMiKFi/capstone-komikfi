35f28dea8b16a6cba517e2a54fe50cdb832b13d1
Yuanhao Zhang <cercle@bu.edu>
2020-02-05 11:38:44 -0500

    beforeMerge

--- components/comicLayout.js
+++ components/comicLayout.js
@@ -1,14 +1,21 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
-import { StyleSheet, View, Text, Image, TouchableOpacity, PixelRatio } from "react-native";
-import { CameraRoll } from 'react-native-cameraroll'
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  PixelRatio
+} from "react-native";
+import { CameraRoll } from "react-native-cameraroll";
 import { connect } from "react-redux";
 import { getPhotoFromLibrary, updateCurrentPhotoIdx } from "../store";
 import PickPhotoPrompt from "./unit/pickPhotoPrompt";
 import SinglePhoto from "./unit/singlePhoto";
-import { captureRef } from  "react-native-view-shot"
+import { captureRef } from "react-native-view-shot";
 import * as Permissions from "expo-permissions";
-import * as MediaLibrary from "expo-media-library"
+import * as MediaLibrary from "expo-media-library";
 
 class ComicLayout extends React.Component {
   constructor() {
@@ -16,36 +23,33 @@ class ComicLayout extends React.Component {
   }
 
   async savePhoto() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
-    console.log(status)
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    console.log(status);
     if (status !== "granted") {
-      alert("The app needs permissions to save to camera roll")
+      alert("The app needs permissions to save to camera roll");
     }
 
     try {
-    console.log("CONTAINER", this.comicView)
+      console.log("CONTAINER", this.comicView);
 
       let uri = await captureRef(this.comicView, {
-      format: 'png',
-      })
+        format: "png"
+      });
 
-      console.log("SHOT", uri)
+      console.log("SHOT", uri);
 
-      const asset = await MediaLibrary.createAssetAsync(uri)
-      console.log("COMIC ASSET:", asset)
+      const asset = await MediaLibrary.createAssetAsync(uri);
+      console.log("COMIC ASSET:", asset);
 
-      const { localUri } = await MediaLibrary.getAssetInfoAsync(asset)
-      console.log("ASSET INFO", localUri)
+      const { localUri } = await MediaLibrary.getAssetInfoAsync(asset);
+      console.log("ASSET INFO", localUri);
 
-
-      let savedComic = await MediaLibrary.saveToLibraryAsync(localUri)
-      console.log("SAVED COMIC", savedComic)
+      let savedComic = await MediaLibrary.saveToLibraryAsync(localUri);
+      console.log("SAVED COMIC", savedComic);
+    } catch (error) {
+      console.log("ERROR IN SAVE PHOTO:", error);
+      console.error(error);
     }
-    catch (error) {
-      console.log("ERROR IN SAVE PHOTO:", error)
-      console.error(error)
-    }
-
 
     // const targetPixelCount = 1080
     // const pixelRatio = PixelRatio.get()
@@ -60,20 +64,25 @@ class ComicLayout extends React.Component {
     // })
   }
 
-
   render() {
     const newArr = new Array(this.props.layout).fill(0);
     return (
       <View style={styles.page}>
         <View
-          ref = {view => {this.comicView = view}}
+          ref={view => {
+            this.comicView = view;
+          }}
           style={styles[`container${this.props.layout}`]}
         >
           {newArr.map((element, index) => {
             return (
               <TouchableOpacity
                 key={index}
-                style={this.props.layout === 4 ? styles.pictureFrame4 : styles.pictureFrame}
+                style={
+                  this.props.layout === 4
+                    ? styles.pictureFrame4
+                    : styles.pictureFrame
+                }
               >
                 {!!this.props.photos[index].image.uri ? (
                   <SinglePhoto
@@ -82,27 +91,26 @@ class ComicLayout extends React.Component {
                     photoIdx={index}
                   />
                 ) : (
-                    <PickPhotoPrompt
-                      navigation={this.props.navigation}
-                      photoIdx={index}
-                    />
-                  )}
+                  <PickPhotoPrompt
+                    navigation={this.props.navigation}
+                    photoIdx={index}
+                  />
+                )}
               </TouchableOpacity>
             );
           })}
         </View>
         <View style={styles.nav}>
-        <TouchableOpacity
-          position={{ x: 0 }}
-          onPress={()=> {this.savePhoto()}}
-        >
-          <Text style={styles.navItem} >Save to Camera Roll</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            position={{ x: 0 }}
+            onPress={() => {
+              this.savePhoto();
+            }}
+          >
+            <Text style={styles.navItem}>Save to Camera Roll</Text>
+          </TouchableOpacity>
         </View>
-
-  </View>
-
-
+      </View>
     );
   }
 }
@@ -112,7 +120,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   container1: {
     flex: 1,
@@ -142,7 +150,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flex: 1,
     flexDirection: "row",
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
     width: "100%",
     alignContent: "stretch",
     alignItems: "flex-start",
@@ -156,7 +164,7 @@ const styles = StyleSheet.create({
     borderColor: "green",
     borderWidth: 5,
     alignItems: "center",
-    justifyContent: "space-around",
+    justifyContent: "space-around"
   },
   pictureFrame4: {
     height: "50%",
@@ -164,13 +172,13 @@ const styles = StyleSheet.create({
     borderColor: "green",
     borderWidth: 5,
     alignItems: "center",
-    justifyContent: "space-around",
+    justifyContent: "space-around"
   },
   image: {
     flex: 1,
     width: "100%",
     borderColor: "green",
-    borderWidth: 5,
+    borderWidth: 5
   },
   nav: {
     flex: 0.1,
@@ -200,11 +208,11 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     getPhotoFromLibrary: async idx => {
       await dispatch(getPhotoFromLibrary(idx));
     },
-    backToEdit: async (index) => {
+    backToEdit: async index => {
       await dispatch(updateCurrentPhotoIdx(index));
       ownProps.navigation.navigate("Edit");
     }
-  }
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ComicLayout);
